8edd0dae69cb86517ba9e1379c3e958f81620ca7
Cleyssonfreitas <cleyssonfreitas30@gmail.com>
2026-03-16 19:56:13 -0300

    Refatorando rotas para a NavigationRoutes

--- app/src/main/java/br/com/fiap/axoeduc/MainActivity.kt
+++ app/src/main/java/br/com/fiap/axoeduc/MainActivity.kt
@@ -14,34 +14,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import br.com.fiap.axoeduc.ui.theme.AxoEducTheme
 import br.com.fiap.axoeduc.components.BottomMenu
 import br.com.fiap.axoeduc.components.CustomTopBar
 import br.com.fiap.axoeduc.navigation.ScreenRoutes
-import br.com.fiap.axoeduc.screens.CursosScreen
-import br.com.fiap.axoeduc.screens.FerramentasScreen
-import br.com.fiap.axoeduc.screens.CalculadoraJurosResultadoScreen
-import br.com.fiap.axoeduc.screens.CalculadoraJurosScreen
-import br.com.fiap.axoeduc.screens.CertificadosScreen
-import br.com.fiap.axoeduc.screens.CofrinhoScreen
-import br.com.fiap.axoeduc.screens.InvestimentosScreen
-import br.com.fiap.axoeduc.screens.LoginScreen
-import br.com.fiap.axoeduc.screens.PerfilScreen
-import br.com.fiap.axoeduc.screens.cadastro.CadastroScreen
-import br.com.fiap.axoeduc.screens.cadastro.CompletarCadastroScreen
+import br.com.fiap.axoeduc.navigation.AppNavigation
 import br.com.fiap.axoeduc.dao.AppDatabase
 import br.com.fiap.axoeduc.repository.UsuarioRepository
-import br.com.fiap.axoeduc.viewmodel.cadastro.CadastroViewModelFactory
-import br.com.fiap.axoeduc.viewmodel.cadastro.CompletarCadastroViewModelFactory
-import br.com.fiap.axoeduc.viewmodel.login.LoginViewModelFactory
-import br.com.fiap.axoeduc.viewmodel.PerfilViewModelFactory
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.platform.LocalContext
 import kotlinx.coroutines.flow.collectLatest
 
@@ -71,8 +52,8 @@ class MainActivity : ComponentActivity() {
 
                 var usuarioLogadoId by remember { mutableIntStateOf(0) }
                 var fotoPerfilUri by remember { mutableStateOf<String?>(null) }
-
                 var nomeUsuarioLogado by remember { mutableStateOf("Aluno") }
+
                 LaunchedEffect(usuarioLogadoId) {
                     if (usuarioLogadoId > 0) {
                         usuarioRepository.buscarPorId(usuarioLogadoId).collectLatest { usuario ->
@@ -110,157 +91,15 @@ class MainActivity : ComponentActivity() {
                     }
                 ) { paddingValues ->
 
-                    NavHost(
+                    AppNavigation(
                         navController = navController,
-                        startDestination = ScreenRoutes.LOGIN,
-                        modifier = Modifier.padding(paddingValues)
-                    ) {
+                        modifier = Modifier.padding(paddingValues),
+                        usuarioRepository = usuarioRepository,
+                        nomeUsuarioLogado = nomeUsuarioLogado,
+                        onUsuarioLogadoChange = { id -> usuarioLogadoId = id },
+                        navegarParaPerfil = { navegarParaPerfil() }
+                    )
 
-                        composable(ScreenRoutes.LOGIN) {
-                            val loginViewModel: br.com.fiap.axoeduc.viewmodel.login.LoginViewModel = viewModel(
-                                factory = LoginViewModelFactory(usuarioRepository)
-                            )
-
-                            LoginScreen(
-                                onLoginSuccess = {
-                                    // Captura o ID do usuário logado antes de navegar
-                                    loginViewModel.usuarioLogadoId?.let { id ->
-                                        usuarioLogadoId = id
-                                    }
-                                    navController.navigate(ScreenRoutes.CURSOS) {
-                                        popUpTo(ScreenRoutes.LOGIN) { inclusive = true }
-                                    }
-                                },
-                                onCriarConta = { navController.navigate(ScreenRoutes.CADASTRO) },
-                                onCadastroIncompleto = { id ->
-                                    usuarioLogadoId = id
-                                    navController.navigate("completar_cadastro/$id") {
-                                        popUpTo(ScreenRoutes.LOGIN) { inclusive = true }
-                                    }
-                                },
-                                viewModel = loginViewModel
-                            )
-                        }
-
-                        composable(ScreenRoutes.CADASTRO) {
-                            CadastroScreen(
-                                onCadastroSucesso = { usuarioId ->
-                                    usuarioLogadoId = usuarioId
-                                    navController.navigate(ScreenRoutes.CURSOS) {
-                                        popUpTo(ScreenRoutes.LOGIN) { inclusive = true }
-                                    }
-                                },
-                                onVoltarLogin = { navController.popBackStack() },
-                                viewModel = viewModel(factory = CadastroViewModelFactory(usuarioRepository))
-                            )
-                        }
-
-                        composable(
-                            route = ScreenRoutes.COMPLETAR_CADASTRO,
-                            arguments = listOf(
-                                navArgument("usuarioId") { type = NavType.IntType }
-                            )
-                        ) { backStackEntry ->
-                            val usuarioId = backStackEntry.arguments?.getInt("usuarioId") ?: 0
-
-                            CompletarCadastroScreen(
-                                onCadastroCompleto = {
-                                    navController.navigate(ScreenRoutes.CURSOS) {
-                                        popUpTo(0) { inclusive = true }
-                                    }
-                                },
-                                viewModel = viewModel(
-                                    factory = CompletarCadastroViewModelFactory(usuarioRepository, usuarioId)
-                                )
-                            )
-                        }
-
-                        composable(
-                            route = ScreenRoutes.PERFIL,
-                            arguments = listOf(
-                                navArgument("usuarioId") { type = NavType.IntType }
-                            )
-                        ) { backStackEntry ->
-                            val usuarioId = backStackEntry.arguments?.getInt("usuarioId") ?: 0
-
-                            PerfilScreen(
-                                onVoltarClick = { navController.popBackStack() },
-                                onSairClick = {
-                                    usuarioLogadoId = 0
-                                    navController.navigate(ScreenRoutes.LOGIN) {
-                                        popUpTo(0) { inclusive = true }
-                                    }
-                                },
-                                viewModel = viewModel(
-                                    factory = PerfilViewModelFactory(usuarioRepository, usuarioId)
-                                )
-                            )
-                        }
-
-                        composable(ScreenRoutes.CURSOS) {
-                            CursosScreen()
-                        }
-
-                        composable(ScreenRoutes.FERRAMENTAS) {
-                            FerramentasScreen(
-                                onProfileClick = { navegarParaPerfil() },
-                                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
-                                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
-                                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) },
-                                onCofrinhoClick = { navController.navigate(ScreenRoutes.COFRINHO) },
-                                onCalculadoraClick = { navController.navigate(ScreenRoutes.CALCULADORA) },
-                                onInvestimentosClick = { navController.navigate(ScreenRoutes.INVESTIMENTOS) }
-                            )
-                        }
-
-                        composable(ScreenRoutes.COFRINHO) {
-                            CofrinhoScreen(
-                                onProfileClick = { navegarParaPerfil() },
-                                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
-                                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
-                                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) }
-                            )
-                        }
-
-                        composable(ScreenRoutes.CALCULADORA) {
-                            CalculadoraJurosScreen(
-                                onProfileClick = { navegarParaPerfil() },
-                                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
-                                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
-                                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) },
-                                onCalcularClick = { _, _ -> navController.navigate(ScreenRoutes.CALCULADORA_RESULTADOS) }
-                            )
-                        }
-
-                        composable(ScreenRoutes.CALCULADORA_RESULTADOS) {
-                            CalculadoraJurosResultadoScreen(
-                                onProfileClick = { navegarParaPerfil() },
-                                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
-                                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
-                                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) }
-                            )
-                        }
-
-                        composable(ScreenRoutes.CERTIFICADOS) {
-                            CertificadosScreen(
-                                nomeUsuario = nomeUsuarioLogado,
-                                onProfileClick = { navegarParaPerfil() },
-                                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
-                                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
-                                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) }
-                            )
-                        }
-
-                        composable(ScreenRoutes.INVESTIMENTOS) {
-                            InvestimentosScreen(
-                                onProfileClick = { navegarParaPerfil() },
-                                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
-                                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
-                                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) },
-                                onInvestimentoClick = { /* TODO futuramente */ }
-                            )
-                        }
-                    }
                 }
             }
         }
--- app/src/main/java/br/com/fiap/axoeduc/navigation/NavigationRoutes.kt
+++ app/src/main/java/br/com/fiap/axoeduc/navigation/NavigationRoutes.kt
@@ -1,1 +1,178 @@
 package br.com.fiap.axoeduc.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import br.com.fiap.axoeduc.repository.UsuarioRepository
+import br.com.fiap.axoeduc.screens.*
+import br.com.fiap.axoeduc.screens.cadastro.*
+import br.com.fiap.axoeduc.viewmodel.PerfilViewModelFactory
+import br.com.fiap.axoeduc.viewmodel.cadastro.CadastroViewModelFactory
+import br.com.fiap.axoeduc.viewmodel.cadastro.CompletarCadastroViewModelFactory
+import br.com.fiap.axoeduc.viewmodel.login.LoginViewModelFactory
+import br.com.fiap.axoeduc.viewmodel.login.LoginViewModel
+
+@Composable
+fun AppNavigation(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    usuarioRepository: UsuarioRepository,
+    nomeUsuarioLogado: String,
+    onUsuarioLogadoChange: (Int) -> Unit,
+    navegarParaPerfil: () -> Unit
+) {
+    NavHost(
+        navController = navController,
+        startDestination = ScreenRoutes.LOGIN,
+        modifier = modifier
+    ) {
+        composable(ScreenRoutes.LOGIN) {
+            val loginViewModel: LoginViewModel = viewModel(
+                factory = LoginViewModelFactory(usuarioRepository)
+            )
+
+            LoginScreen(
+                onLoginSuccess = {
+                    loginViewModel.usuarioLogadoId?.let { id ->
+                        onUsuarioLogadoChange(id)
+                    }
+                    navController.navigate(ScreenRoutes.CURSOS) {
+                        popUpTo(ScreenRoutes.LOGIN) { inclusive = true }
+                    }
+                },
+                onCriarConta = { navController.navigate(ScreenRoutes.CADASTRO) },
+                onCadastroIncompleto = { id ->
+                    onUsuarioLogadoChange(id)
+                    navController.navigate("completar_cadastro/$id") {
+                        popUpTo(ScreenRoutes.LOGIN) { inclusive = true }
+                    }
+                },
+                viewModel = loginViewModel
+            )
+        }
+
+        composable(ScreenRoutes.CADASTRO) {
+            CadastroScreen(
+                onCadastroSucesso = { usuarioId ->
+                    onUsuarioLogadoChange(usuarioId)
+                    navController.navigate(ScreenRoutes.CURSOS) {
+                        popUpTo(ScreenRoutes.LOGIN) { inclusive = true }
+                    }
+                },
+                onVoltarLogin = { navController.popBackStack() },
+                viewModel = viewModel(factory = CadastroViewModelFactory(usuarioRepository))
+            )
+        }
+
+        composable(
+            route = ScreenRoutes.COMPLETAR_CADASTRO,
+            arguments = listOf(
+                navArgument("usuarioId") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val usuarioId = backStackEntry.arguments?.getInt("usuarioId") ?: 0
+
+            CompletarCadastroScreen(
+                onCadastroCompleto = {
+                    navController.navigate(ScreenRoutes.CURSOS) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                },
+                viewModel = viewModel(
+                    factory = CompletarCadastroViewModelFactory(usuarioRepository, usuarioId)
+                )
+            )
+        }
+
+        composable(
+            route = ScreenRoutes.PERFIL,
+            arguments = listOf(
+                navArgument("usuarioId") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val usuarioId = backStackEntry.arguments?.getInt("usuarioId") ?: 0
+
+            PerfilScreen(
+                onVoltarClick = { navController.popBackStack() },
+                onSairClick = {
+                    onUsuarioLogadoChange(0) // Zera o ID ao sair
+                    navController.navigate(ScreenRoutes.LOGIN) {
+                        popUpTo(0) { inclusive = true }
+                    }
+                },
+                viewModel = viewModel(
+                    factory = PerfilViewModelFactory(usuarioRepository, usuarioId)
+                )
+            )
+        }
+
+        composable(ScreenRoutes.CURSOS) {
+            CursosScreen()
+        }
+
+        composable(ScreenRoutes.FERRAMENTAS) {
+            FerramentasScreen(
+                onProfileClick = { navegarParaPerfil() },
+                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
+                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
+                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) },
+                onCofrinhoClick = { navController.navigate(ScreenRoutes.COFRINHO) },
+                onCalculadoraClick = { navController.navigate(ScreenRoutes.CALCULADORA) },
+                onInvestimentosClick = { navController.navigate(ScreenRoutes.INVESTIMENTOS) }
+            )
+        }
+
+        composable(ScreenRoutes.COFRINHO) {
+            CofrinhoScreen(
+                onProfileClick = { navegarParaPerfil() },
+                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
+                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
+                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) }
+            )
+        }
+
+        composable(ScreenRoutes.CALCULADORA) {
+            CalculadoraJurosScreen(
+                onProfileClick = { navegarParaPerfil() },
+                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
+                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
+                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) },
+                onCalcularClick = { _, _ -> navController.navigate(ScreenRoutes.CALCULADORA_RESULTADOS) }
+            )
+        }
+
+        composable(ScreenRoutes.CALCULADORA_RESULTADOS) {
+            CalculadoraJurosResultadoScreen(
+                onProfileClick = { navegarParaPerfil() },
+                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
+                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
+                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) }
+            )
+        }
+
+        composable(ScreenRoutes.CERTIFICADOS) {
+            CertificadosScreen(
+                nomeUsuario = nomeUsuarioLogado,
+                onProfileClick = { navegarParaPerfil() },
+                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
+                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
+                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) }
+            )
+        }
+
+        composable(ScreenRoutes.INVESTIMENTOS) {
+            InvestimentosScreen(
+                onProfileClick = { navegarParaPerfil() },
+                onCursosClick = { navController.navigate(ScreenRoutes.CURSOS) },
+                onFerramentasClick = { navController.navigate(ScreenRoutes.FERRAMENTAS) },
+                onCertificadosClick = { navController.navigate(ScreenRoutes.CERTIFICADOS) },
+                onInvestimentoClick = { /* TODO futuramente */ }
+            )
+        }
+    }
+}
